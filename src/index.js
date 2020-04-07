b8dad7ba738cae3d6e5155fef7428b7d4c67aca4
Saad Asif <saadasif2121@gmail.com>
2020-04-06 22:02:45 -0700

    adding https redirects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <HttpsRedirect>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HttpsRedirect>,
   document.getElementById('root')
 );
