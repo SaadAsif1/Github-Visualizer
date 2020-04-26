abe2d831af9d1fad0c5241817a994b3a52c732dd
Saad Asif <saadasif2121@gmail.com>
2020-04-26 08:54:54 -0700

    add ssl redirect

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 8080;
 const app = express();
+
+// enable ssl redirect
+app.use(sslRedirect());
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
